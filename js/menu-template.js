486eeb7fa0f1679c1e7151bc26b197c86adfefac
Patrick Liang <patrick.liang.7@gmail.com>
2013-11-05 02:06:49 -0800

    Added menu JavaScript for data structure

---
+++ js/menu-template.js
@@ -0,0 +1,15 @@
+function pizzaDisplay(menu) {
+	var idx;
+	var pizza;
+	for (idx = 0; idx < com.dawgpizza.menu.pizzas.length; ++idx) {
+	    pizza = com.dawgpizza.menu.pizzas[idx];
+
+	    //pizza.name = name of pizza
+	    //pizza.description = description of pizza
+	    //pizza.prices = array of three numbers, which are prices for small, medium, and large
+	    //pizza.prices[0] = price for small
+	    //pizza.prices[1] = price for medium
+	    //pizza.prices[2] = price for large
+
+	} //for each pizza
+}

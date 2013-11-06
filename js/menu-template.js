9f39f2104ddd28253a2790ec41b90c3f4a48636c
Patrick Liang <patrick.liang.7@gmail.com>
2013-11-06 02:39:00 -0800

    Changed to data structure template to display menu

--- js/menu-template.js
+++ js/menu-template.js
@@ -1,8 +1,24 @@
 function pizzaDisplay(menu) {
 	var idx;
 	var pizza;
+	var template;
+	var pizzaMenu;
+	var instance;
+
+	template = $('.template');
+	pizzaMenu = $('.pizza-menu');
+	pizzaMenu.empty();
+
 	for (idx = 0; idx < com.dawgpizza.menu.pizzas.length; ++idx) {
+		instance = template.clone();
 	    pizza = com.dawgpizza.menu.pizzas[idx];
+	    instance.find('.name').html(pizza.name);
+	    instance.find('.description').html(pizza.description);
+	    instance.find('.small-price').html("$" + pizza.prices[0] + "/");
+	    instance.find('.medium-price').html("$" + pizza.prices[1] + "/");
+	    instance.find('.large-price').html("$" + pizza.prices[2]);
+	    instance.removeClass('template');
+	    pizzaMenu.append(instance);
 
 	    //pizza.name = name of pizza
 	    //pizza.description = description of pizza
@@ -13,3 +29,55 @@ function pizzaDisplay(menu) {
 
 	} //for each pizza
 }
+
+function drinkDisplay(menu) {
+	var idx;
+	var drink;
+	var template;
+	var drinkMenu;
+	var instance;
+
+	template = $('.template');
+	drinkMenu = $('.drink-menu');
+	drinkMenu.empty();
+
+	for (idx = 0; idx < com.dawgpizza.menu.drinks.length; ++idx) {
+		instance = template.clone();
+	    drink = com.dawgpizza.menu.drinks[idx];
+	    instance.find('.drink-name').html(drink.name);
+	    instance.find('.drink-price').html("$" + drink.price);
+	    instance.removeClass('template');
+	    drinkMenu.append(instance);
+
+	    //drink.name = name of drink
+	    //drink.price = price of drink
+
+	} //for each drink
+}
+
+function dessertDisplay(menu) {
+	var idx;
+	var dessert;
+	var template;
+	var dessertMenu;
+	var instance;
+
+	template = $('.template');
+	dessertMenu = $('.dessert-menu');
+	dessertMenu.empty();
+
+	for (idx = 0; idx < com.dawgpizza.menu.desserts.length; ++idx) {
+		instance = template.clone();
+	    dessert = com.dawgpizza.menu.desserts[idx];
+	    instance.find('.dessert-name').html(dessert.name);
+	    instance.find('.dessert-price').html("$" + dessert.price);
+	    instance.removeClass('template');
+	    dessertMenu.append(instance);
+	}
+}
+
+$(function() {
+	pizzaDisplay(com.dawgpizza.menu);
+	drinkDisplay(com.dawgpizza.menu);
+	dessertDisplay(com.dawgpizza.menu);
+});
